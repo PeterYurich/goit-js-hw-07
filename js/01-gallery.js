999a0c80848a07fbf81301d905a5b505917e7b6c
Peter <peteryurich@gmail.com>
2022-07-19 00:08:07 +0300

    1.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// 1. создание разметки
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const readyGallery = galleryItems.reduce((stringAcc, imgItem) => {
+    return stringAcc + `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+        <img
+        class="gallery__image"
+        src="${imgItem.preview}"
+        data-source="${imgItem.original}"
+        alt="${imgItem.description}"
+        />
+    </a>
+    </div>`;
+
+
+}, "")
+
+galleryEl.insertAdjacentHTML('afterbegin', readyGallery);
+
+// 2. делегирование
+
+galleryEl.addEventListener('click', onPicture);
+
+function onPicture(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== "A") { return }
+
+    const pictureUrl = e.target.dataset.source
+    return console.log(pictureUrl)
+}
